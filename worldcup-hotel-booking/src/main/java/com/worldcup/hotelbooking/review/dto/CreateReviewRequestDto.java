e396a45d3a486f3054be79b002c1b585fc79770c
Qasem Alkurdi <7ksh1970@gmail.com>
2026-03-22 18:44:00 +0200

    refactor(review): format CreateReviewRequestDto for improved readability
    Signed-off-by: Qasem Alkurdi <7ksh1970@gmail.com>

--- worldcup-hotel-booking/src/main/java/com/worldcup/hotelbooking/review/dto/CreateReviewRequestDto.java
+++ worldcup-hotel-booking/src/main/java/com/worldcup/hotelbooking/review/dto/CreateReviewRequestDto.java
@@ -3,9 +3,9 @@ package com.worldcup.hotelbooking.review.dto;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 
-@AllArgsConstructor
-public record CreateReviewRequestDto(@NotNull @Min(1) @Max(5) Integer rating, String comment) {
-
+public record CreateReviewRequestDto(
+        @NotNull @Min(1) @Max(5) Integer rating,
+        String comment
+) {
 }
